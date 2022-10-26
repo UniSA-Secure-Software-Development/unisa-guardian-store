f441fcf9a348513a4de343ce5ffd2f6ea11cf956
Natalie Watkins <watns001@mymail.unisa.edu.au>
2022-10-27 00:42:46 +1030

    Fix for Typescript syntax error on build

--- routes/wallet.ts
+++ routes/wallet.ts
@@ -23,7 +23,8 @@ module.exports.addWalletBalance = function addWalletBalance () {
     const cardId = req.body.paymentId
     const card = cardId ? await CardModel.findOne({ where: { id: cardId, UserId: req.body.UserId } }) : null
     if (card) {
-      const wallet = await WalletModel.increment({ balance: req.body.balance }, { where: { UserId: req.body.UserId } })
+      await WalletModel.increment({ balance: req.body.balance }, { where: { UserId: req.body.UserId } })
+      const wallet = await WalletModel.findOne({ where: { UserId: req.body.UserId} })
       if (wallet) {
         res.status(200).json({ status: 'success', data: wallet.balance })
       } else {
