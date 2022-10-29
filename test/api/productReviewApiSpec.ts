c2c5d6fc5ea581b81b9ae140692f2e40e922c85b
Raymond Stoddard <storg001@mymail.unisa.edu.au>
2022-10-30 07:45:27 +1030

    change order

--- test/api/productReviewApiSpec.ts
+++ test/api/productReviewApiSpec.ts
@@ -12,7 +12,7 @@ const http = require('http')
 const REST_URL = 'http://localhost:3000/rest'
 
 const jsonHeader = { 'content-type': 'application/json' }
-let authHeader = { Authorization: `Bearer ${security.authorize()}`, 'content-type': 'application/json' }
+const authHeader = { Authorization: `Bearer ${security.authorize()}`, 'content-type': 'application/json' }
 
 describe('/rest/products/:id/reviews', () => {
   const reviewResponseSchema = {
