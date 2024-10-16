dc07e3ca60293a37cfd1b2144d27eb51ec0086a0
Anh <nguay089@mymail.unisa.edu.au>
2024-10-16 15:04:22 +1030

    test 1

--- data/static/codefixes/forgedReviewChallenge_1.ts
+++ data/static/codefixes/forgedReviewChallenge_1.ts
@@ -2,7 +2,9 @@ module.exports = function productReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
     const user = security.authenticatedUsers.from(req)
     db.reviews.update(
-      { _id: req.body.id },
+    //"Assigning the author on the server side based on the user identified from the authentication token in the HTTP request is the correct approach. 
+    //This ensures that users cannot simply include any author email of their choice in the request."
+      { _id: req.body.id, author: user.data.email },
       { $set: { message: req.body.message, author: user.data.email } },
       { multi: true }
     ).then(
