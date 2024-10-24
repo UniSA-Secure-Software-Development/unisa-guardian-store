dd323522060ea4b1758f025788a9daba77ac72ef
Diep Nhat An Andrew Tran <trady052@mymail.unisa.edu.au>
2024-10-24 18:33:29 +1030

    testing bearer scan

--- routes/search.ts
+++ routes/search.ts
@@ -20,9 +20,11 @@ module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query('SELECT * FROM Products WHERE ((name LIKE $1 OR description LIKE $1) AND deletedAt IS NULL) ORDER BY name', {
-      bind: [`%${criteria}%`]
-    }) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+    let query: String = `SELECT * FROM Products WHERE name LIKE '%${criteria}%'`
+    models.sequelize.query(query)
+//    models.sequelize.query('SELECT * FROM Products WHERE ((name LIKE $1 OR description LIKE $1) AND deletedAt IS NULL) ORDER BY name', {
+//      bind: [`%${criteria}%`]
+//    }) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
