f4938d10b2eb4f276414f241aeaa0dec6d5e865d
Zuriel Villarino <vilzd001@mymail.unisa.edu.au>
2024-10-28 06:41:10 +0000

    Completed assignment submission

--- routes/login.ts
+++ routes/login.ts
@@ -33,27 +33,35 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-      .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-        const user = utils.queryResultToJson(authenticatedUser)
-        if (user.data?.id && user.data.totpSecret !== '') {
-          res.status(401).json({
-            status: 'totp_token_required',
-            data: {
-              tmpToken: security.authorize({
-                userId: user.data.id,
-                type: 'password_valid_needs_second_factor_token'
-              })
-            }
-          })
-        } else if (user.data?.id) {
-          afterLogin(user, res, next)
-        } else {
-          res.status(401).send(res.__('Invalid email or password.'))
-        }
-      }).catch((error: Error) => {
-        next(error)
-      })
+    const email = req.body.email || ''
+    const password = security.hash(req.body.password || '')
+    models.sequelize.query(
+      'SELECT * FROM Users WHERE email = :email AND password = :password AND deletedAt IS NULL',
+      {
+        replacements: { email, password },
+        model: UserModel,
+        plain: true
+      }
+    ).then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+      const user = utils.queryResultToJson(authenticatedUser)
+      if (user.data?.id && user.data.totpSecret !== '') {
+        res.status(401).json({
+          status: 'totp_token_required',
+          data: {
+            tmpToken: security.authorize({
+              userId: user.data.id,
+              type: 'password_valid_needs_second_factor_token'
+            })
+          }
+        })
+      } else if (user.data?.id) {
+        afterLogin(user, res, next)
+      } else {
+        res.status(401).send(res.__('Invalid email or password.'))
+      }
+    }).catch((error: Error) => {
+      next(error)
+    })
   }
   // vuln-code-snippet end loginAdminChallenge loginBenderChallenge loginJimChallenge
 
--- routes/updateProductReviews.ts
+++ routes/updateProductReviews.ts
@@ -13,19 +13,28 @@ const security = require('../lib/insecurity')
 // vuln-code-snippet start noSqlReviewsChallenge forgedReviewChallenge
 module.exports = function productReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
+    const id = req.body.id
+    const message = req.body.messagee
     const user = security.authenticatedUsers.from(req) // vuln-code-snippet vuln-line forgedReviewChallenge
-    db.reviews.update( // vuln-code-snippet neutral-line forgedReviewChallenge
-      { _id: req.body.id }, // vuln-code-snippet vuln-line noSqlReviewsChallenge forgedReviewChallenge
-      { $set: { message: req.body.message } },
-      { multi: true } // vuln-code-snippet vuln-line noSqlReviewsChallenge
-    ).then(
-      (result: { modified: number, original: Array<{ author: any }> }) => {
-        challengeUtils.solveIf(challenges.noSqlReviewsChallenge, () => { return result.modified > 1 }) // vuln-code-snippet hide-line
-        challengeUtils.solveIf(challenges.forgedReviewChallenge, () => { return user?.data && result.original[0] && result.original[0].author !== user.data.email && result.modified === 1 }) // vuln-code-snippet hide-line
-        res.json(result)
-      }, (err: unknown) => {
-        res.status(500).json(err)
-      })
+
+    // adding if statement for valid input validation
+    if (/^[0-9a-fA-F]{24}$/.test(id) && typeof message === 'string') {
+      db.reviews.update( // vuln-code-snippet neutral-line forgedReviewChallenge
+        { _id: id }, // vuln-code-snippet vuln-line noSqlReviewsChallenge forgedReviewChallenge
+        { $set: { message: message } },
+        { multi: true } // vuln-code-snippet vuln-line noSqlReviewsChallenge
+      ).then(
+        (result: { modified: number, original: Array<{ author: any }> }) => {
+          challengeUtils.solveIf(challenges.noSqlReviewsChallenge, () => { return result.modified > 1 }) // vuln-code-snippet hide-line
+          challengeUtils.solveIf(challenges.forgedReviewChallenge, () => { return user?.data && result.original[0] && result.original[0].author !== user.data.email && result.modified === 1 }) // vuln-code-snippet hide-line
+          res.json(result)
+        }, (err: unknown) => {
+          res.status(500).json(err)
+        })
+    } else {
+      // handling unautroized access or invalid input
+      res.status(400).send('Invaliod input or authorized access')
+    }
   }
 }
 // vuln-code-snippet end noSqlReviewsChallenge forgedReviewChallenge
