d0092444aa60c306f138efeb1cb4bbc72ebced7b
glassesarms <ethan@glassarms.com>
2024-10-28 21:10:07 +1030

    fix linting issues

--- frontend/src/app/helpers/security-helper.ts
+++ frontend/src/app/helpers/security-helper.ts
@@ -1,16 +1,16 @@
-import { DomSanitizer } from '@angular/platform-browser';
-import { SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser'
+import { SecurityContext } from '@angular/core'
 
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class SecurityHelper {
+  private static encodeHTML (value: string): string {
+    const element = document.createElement('div')
+    element.innerText = value
+    return element.innerHTML
+  }
 
-    private static encodeHTML (value: string): string {
-        const element = document.createElement('div')
-        element.innerText = value
-        return element.innerHTML
-    }
-    
-    static sanitizeAndEncodeInputForHTML (sanitizer: DomSanitizer, value: string): string {
-        const sanitizedInput = sanitizer.sanitize(SecurityContext.HTML, value) || ''
-        return this.encodeHTML(sanitizedInput)
-    }
+  static sanitizeAndEncodeInputForHTML (sanitizer: DomSanitizer, value: string): string {
+    const sanitizedInput = sanitizer.sanitize(SecurityContext.HTML, value) || ''
+    return this.encodeHTML(sanitizedInput)
+  }
 }
