57a1834150f8623f959347fde47554a541d745d8
Kata K <kowky004@mymail.unisa.edu.au>
2024-11-02 19:17:39 +1030

    Repair SQL Injection Vulnerabilities
    Fix hole in login.ts and search.ts

--- routes/login.ts
+++ routes/login.ts
@@ -33,8 +33,8 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-      .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    UserModel.findOne({ where: { email: req.body.email, password: security.hash(req.body.password) } })
+      .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
           res.status(401).json({
@@ -47,7 +47,11 @@ module.exports = function login () {
             }
           })
         } else if (user.data?.id) {
-          afterLogin(user, res, next)
+          if (!user.data?.deletedFlag) {
+            afterLogin(user, res, next)
+          } else {
+            res.status(401).send(res.__('Invalid email or password.'))
+          }
         } else {
           res.status(401).send(res.__('Invalid email or password.'))
         }
