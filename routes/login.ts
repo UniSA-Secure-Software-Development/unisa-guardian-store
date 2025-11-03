f7b761d0164e13d6cc42deaa9d0f4da18827c211
Winterhuli <heycy013@mymail.unisa.edu.au>
2025-11-03 18:17:38 +1030

    Fix: use parameterized queries in login and search to prevent SQLi

--- routes/login.ts
+++ routes/login.ts
@@ -33,7 +33,15 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    const email = String(req.body.email || '')
+    const passwordHash = security.hash(req.body.password || '')
+    models.sequelize.query('SELECT * FROM Users WHERE email = :email AND password = :password AND deletedAt IS NULL',
+      {
+        replacements: { email, password: passwordHash },
+        model: UserModel,
+        plain: true
+      }
+    ) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
       .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
