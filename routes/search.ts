d86549a29c621022bceaf094099381893c394efd
goyry006 <goyry006@mymail.unisa.edu.au>
2025-11-07 23:38:27 +1030

    Completed assignment submission

--- routes/search.ts
+++ routes/search.ts
@@ -19,9 +19,18 @@ class ErrorWithParent extends Error {
 module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
-      .then(([products]: any) => {
+    criteria = (typeof criteria === 'string') ? criteria.trim() : ''
+    if (criteria.length > 200) criteria = criteria.substring(0, 200)
+    if (!/^[\w\s\-.,&()!?:'"]{0,200}$/.test(criteria)) {
+      return res.status(400).json({ error: 'Invalid search input' })
+    }
+    models.sequelize.query('SELECT * FROM Products WHERE ((name LIKE :c OR description LIKE :c) AND deletedAt IS NULL) ORDER BY name',
+      {
+        replacements: { c: `%${criteria}%` },
+
+        type: models.sequelize.QueryTypes.SELECT
+      }) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+      .then((products: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
           let solved = true
